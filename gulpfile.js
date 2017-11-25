2d858cd836b2c0bd3d5f925b117cdc205ff828f6
Patrique Beaupain <patriquebeaupain@Patriques-MacBook-Pro.local>
2017-11-25 15:25:39 +0100

    added bootstrap and concat the css files

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
+var merge = require('merge-stream');
 
 var SOURCEPATHS = {
 	sassSource : 'src/scss/*.scss',
@@ -29,15 +31,22 @@ gulp.task('clean-scripts', function() {
 });
 
 gulp.task('sass', function() {
-	return gulp.src (SOURCEPATHS.sassSource)
+	var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+	var sassFiles;
+
+
+	sassFiles = gulp.src (SOURCEPATHS.sassSource)
 		.pipe(autoprefixer())
 		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-		.pipe(gulp.dest(APPATH.css));
+		return merge(bootstrapCSS, sassFiles)
+			.pipe(concat('app.css'))
+			.pipe(gulp.dest(APPATH.css));
 });
-
+ 
 gulp.task('scripts', ['clean-scripts'], function() {
 	gulp.src(SOURCEPATHS.jsSource)
 		.pipe(concat('main.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest(APPATH.js))
 });
 
---
+++ src/js/scripts.js
@@ -0,0 +1,7 @@
+global.jQuery = require('jQuery');
+bootstrap = require('bootstrap');
+mustache = require('mustache');
+
+jQuery(document).ready(function() {
+	alert("hello");
+});
